cd536ab968227285723309e29ee1a970126bf258
Elpylol <karatel-elmore@mail.ru>
2016-11-21 19:43:16 +0300

    Проверка2

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@ close.addEventListener("click", function (event) {
   event.preventDefault();
   popup.classList.remove("active");
 });
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.querySelector(‘.map'), {
+    center: {lat: 59.938976, lng: 30.323422},
+    zoom: 17,
+    disableDefaultUI: true,
+    scrollwheel:  false,
+    zoomControl: true
+  });
+  var image = 'img/map-marker.png';
+  var studioMarker = new google.maps.Marker({
+    position: {lat: 59.938645, lng: 30.323087},
+    map: map,
+    icon: image
+  });
+}
